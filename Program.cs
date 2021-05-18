f25da2e27295c8df5c0793bfc65e97ed105cf389
Grant <grantferment@gmail.com>
2021-05-18 17:22:19 -0400

    Sorting and searching using .IndexOf

--- Program.cs
+++ Program.cs
@@ -15,13 +15,37 @@ namespace ConsoleApp1
             names.Add("Maria");
             names.Add("Bill");
             names.Remove("Ana");
+
             foreach (var name in names)
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
             Console.WriteLine($"My name is {names[1]}");
             Console.WriteLine($"I've added {names[2]}, {names[3]} and {names[4]} to the list");
             Console.WriteLine($"The list has {names.Count} people in it");
+            
+            
+            var index = names.IndexOf("Felipe");
+                if (index == -1)
+                {
+                    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+                }
+                else
+                {
+                    Console.WriteLine($"The name {names[index]} is at index {index}");
+                }
+
+            index = names.IndexOf("Not Found");
+                if (index == -1)
+                {
+                    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+                }
+                else
+                {
+                    Console.WriteLine($"The name {names[index]} is at index {index}");
+
+                }
         }
     }
 }
