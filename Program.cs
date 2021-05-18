af9706c6dfb70cdc60049a6f4f3436a309169bb7
Grant <grantferment@gmail.com>
2021-05-18 18:07:54 -0400

    Fibonacci Sequence using C#

--- Program.cs
+++ Program.cs
@@ -7,45 +7,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var names = new List<string>
-                {
-                    "<name>", "Ana", "Felipe"
-                };
-            names.Add("Grant");
-            names.Add("Maria");
-            names.Add("Bill");
-            names.Remove("Ana");
+            var fibonacciNumbers = new List<int> { 1, 1 };
 
-            foreach (var name in names)
+            // var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            // var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            while (fibonacciNumbers.Count < 20)
             {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
+                fibonacciNumbers.Add(fibonacciNumbers[fibonacciNumbers.Count - 1] + fibonacciNumbers[fibonacciNumbers.Count - 2]);
             }
 
-            Console.WriteLine($"My name is {names[1]}");
-            Console.WriteLine($"I've added {names[2]}, {names[3]} and {names[4]} to the list");
-            Console.WriteLine($"The list has {names.Count} people in it");
-            
-            
-            var index = names.IndexOf("Felipe");
-                if (index == -1)
-                {
-                    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
-                }
-                else
-                {
-                    Console.WriteLine($"The name {names[index]} is at index {index}");
-                }
-
-            index = names.IndexOf("Not Found");
-                if (index == -1)
-                {
-                    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
-                }
-                else
-                {
-                    Console.WriteLine($"The name {names[index]} is at index {index}");
-
-                }
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
     }
 }
